a8bf909a633fa9a079d9d62d65e1b36359ebc94f
NickTheo96 <nicktheodoulou96@gmail.com>
2018-09-05 14:04:08 +0100

    -Created a non-generalised plot in main that changes the colour of the data point depending on the exterior quality, need to generalise this to n classifications for all the attributes -RoofStyle graph also plotted where classifications arent in order, need to find a suitable way to order each different attribute that is a string

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def main():
     #matrix.sale_price_against_attribute_scatter_plot('TotalBsmtSF')
     #matrix.sale_price_against_attribute_scatter_plot('MSSubClass')
 
-    #print(Counter(matrix._train["ExterQual"]))#prints the number of each classification for ExterQual
+    print(Counter(matrix._train["ExterQual"]))#prints the number of each classification for ExterQual
 
     # print(train.head())#prints the first 5 columns of train
     # np.savetxt('Test/First_Floor_Sq_Foot.out', train['1stFlrSF'].values, delimiter=',')  # creates a file so that the data is easier to look at
@@ -23,6 +23,15 @@ def main():
 
     #sale_price_against_attribute_scatter_plot('1stFlrSF')  # function that takes in a column name of train as an attribute and creates a scatter plot of
     # the sale price against the attribute
+
+
+
+
+    ##################################################################################################################################################################
+    #This attribute is already ordered as excellent, good, average, fair, poor can be converted to 5,4,3,2,1 easily
+
+
+
 
     list_exterior_quality = matrix._train['ExterQual'].values
     list_colour_corresponding_to_exterior_quality = [0] * len(matrix._train)
@@ -52,13 +61,70 @@ def main():
     plt.scatter(matrix._train['1stFlrSF'].values, matrix._train['SalePrice'].values, color=list_colour_corresponding_to_exterior_quality)
     plt.xlabel('1stFlrSF')
     plt.ylabel('SalePrice')
-
-    for i in range(0, 5):# plot empty lists with the desired size and label to creat the legend
+    for i in range(0, len(list_of_attributes)):# plot empty lists with the desired size and label to creat the legend (not possible any other way)
         plt.scatter([], [], c=list_of_colours[i], alpha=1, label=list_of_attributes[i])
 
-    plt.legend(loc=0, scatterpoints=1, frameon=False, labelspacing=0, title='Exterior Quality: ', prop={'size': 9})
+    plt.legend(loc=0, scatterpoints=1, frameon=False, labelspacing=0, title='RoofStyle: ', prop={'size': 9})
     plt.show()
 
+
+    ###################################################################################################################################################################################################
+
+
+
+
+
+
+
+    ########################################################################################################################################################################################################################
+    # This attribute is not ordered so it is not as simple to conver to 6,5,4,3,2,1
+
+
+    list_Roof_Style = matrix._train['RoofStyle'].values#define a column of all the roof types
+    list_colour_corresponding_to_roof_style = [0] * len(matrix._train)#used when later converting the strings to int
+    list_of_colours = ['r', 'b', 'g', 'y', 'm','c']#colours used to visualise the data
+    list_of_attributes = ['Flat', 'Gable', 'Gambrel', 'Hip', 'Mansard', 'Shed']#the ith colour corresponds to the ith type of roof
+
+    for i in range(0, len(matrix._train)):
+        if list_Roof_Style[i] == 'Flat':
+            list_Roof_Style[i] = 6 #this converts the style to an int however as I do not know the order of which roof style is more desireable than the other,
+            #cant simply just rank them alphabetically
+            list_colour_corresponding_to_roof_style[i] = "r" #this is the loop that defines the coorect colour for each roof style
+        elif list_Roof_Style[i] == 'Gable':
+            list_Roof_Style[i] = 5
+            list_colour_corresponding_to_roof_style[i] = "b"
+        elif list_Roof_Style[i] == 'Gambrel':
+            list_Roof_Style[i] = 4
+            list_colour_corresponding_to_roof_style[i] = "g"
+        elif list_Roof_Style[i] == 'Hip':
+            list_Roof_Style[i] = 3
+            list_colour_corresponding_to_roof_style[i] = "y"
+        elif list_Roof_Style[i] == 'Mansard':
+            list_Roof_Style[i] = 2
+            list_colour_corresponding_to_roof_style[i] = "m"
+        elif list_Roof_Style[i] == 'Shed':
+            list_Roof_Style[i] = 1
+            list_colour_corresponding_to_roof_style[i] = "c"
+        else:
+            print("Error")#break if theres an error
+            break
+
+    plt.scatter(matrix._train['1stFlrSF'].values, matrix._train['SalePrice'].values, color=list_colour_corresponding_to_roof_style)#creates the scatter plot
+    #where colour contributes to the roof style
+    plt.xlabel('1stFlrSF')
+    plt.ylabel('SalePrice')
+
+    for i in range(0, len(list_of_attributes)):# plot empty lists with the desired size and label to creat the legend (not possible any other way)
+        plt.scatter([], [], c=list_of_colours[i], alpha=1, label=list_of_attributes[i])
+
+    plt.legend(loc=0, scatterpoints=1, frameon=False, labelspacing=0, title='RoofStyle: ', prop={'size': 9})
+    plt.show()
+
+    ##################################################################################################################################################################
+
+
+
+    print(Counter(matrix._train["RoofStyle"]))  # prints the number of each classification for ExterQual
 
 if __name__ == "__main__":
     main()
