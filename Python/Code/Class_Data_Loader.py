66147a77fa46d927ba21ded0466793539346f660
NickTheo96 <nicktheodoulou96@gmail.com>
2018-10-12 12:34:15 +0100

    - Need to verify that all the Data_In files contain the same data - Need to see how the sample size varies year to year

--- Python/Code/Class_Data_Loader.py
+++ Python/Code/Class_Data_Loader.py
@@ -16,7 +16,3 @@ class DataLoader:  # class that loads in the data
 
     def dim_data(self):  # function that prints the dimensions of the dataset
         print('The dimensions of the dataset is', self._dataset.shape)
-
-    def split_month_year(self, date_column_label):  # function that creates 2 new columns for both year and month
-        self._dataset['Year'] = pd.DatetimeIndex(self._dataset[date_column_label]).year
-        self._dataset['Month'] = pd.DatetimeIndex(self._dataset[date_column_label]).month
--- Python/Code/main.py
+++ Python/Code/main.py
@@ -9,16 +9,9 @@ def main():
     print(model_df.dim_data())  # prints the dimension of the data set
     model_df.missing_data_ratio_print() #  prints the percentage of missing values in the data set (NONE FOUND!)
 
-
-    # method that creates two new columns in the data frame for the year and month from the column Financial Year
-    model_df.split_month_year("FinancialYear")
-
-    print(model_df._dataset.head())  # prints the first 5 columns of the data-set after adding 2 new columns
-
     model_df._dataset.to_csv("Data_Out/out.csv") #  saves the pandas data frame to a CSV file
 
-    model_df.box_plot("ConditionScore", "Year")  # box plot of the Condition score for each Year
-    model_df.box_plot("ConditionScore", "Month")  # box plot of the Condition score for each Month
+    model_df.box_plot("ConditionScore", "FinancialYear")  # box plot of the Condition score for each Year
 
 
 if __name__ == "__main__":
