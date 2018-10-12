3df4ec3b91ba73350b6a9348bedec92931c38c71
NickTheo96 <nicktheodoulou96@gmail.com>
2018-10-12 17:24:24 +0100

    - Need to verify that all the Data_In files contain the same data Done - Need to put percentage change as a function - Added gitignore file

--- Python/Code/main.py
+++ Python/Code/main.py
@@ -23,30 +23,9 @@ def main():
     #  function that plots the percentage of what year each inspection occurs.
     model_df.bar_graph_percentage("Year")
 
-###################################################################################
-# need to move into function within exploration
-    FinancialYearCount = model_df._data_set["FinancialYear"].value_counts().sort_index()
+    #  function that plots the percentage difference of what year each inspection occurs.
+    model_df.bar_graph_percentage_difference("FinancialYear")
 
-    percentage_change = np.zeros(FinancialYearCount.count() - 1)
-
-    for i in range(0, FinancialYearCount.count() - 1):  # for loop to calculate the percentage difference
-        percentage_change[i] = ((FinancialYearCount.values[i+1] - FinancialYearCount.values[i]) / FinancialYearCount.values[i]) * 100
-
-    print(percentage_change)
-
-
-    x = FinancialYearCount[:-1].index #drops the final column in the series object and sets x to the index
-    plt.subplots(figsize=(16, 8))  # changes the size of the fig
-    plt.grid()
-    plt.plot(x, percentage_change, c="g", alpha=0.5, marker="s")
-    #  plt.title('Bar graph of ' + str(column_count.name) + ' Against ' + str(' Sample Size'), fontsize=20)
-    plt.xlabel(FinancialYearCount.name, fontsize=24)  # sets the xlabel to the name of the series object
-    plt.ylabel('Percent Change', fontsize=24)
-    plt.xticks(x, rotation=30)  # rotates ticks by 90deg so larger font can be used
-    plt.tick_params(labelsize=22)  # increases font of the ticks
-    plt.show()
-
-##########################################################################################
 
 if __name__ == "__main__":
     main()
