8342223eb0a370735d8beecf80a80819f6804511
NickTheo96 <nicktheodoulou96@gmail.com>
2018-10-15 13:18:08 +0100

    - Need to check all data in files are the same - Generalised triple stacked bar graph

--- Python/main.py
+++ Python/main.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from Python.Class_Data_Exploration import DataExploration
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 def main():
@@ -9,12 +11,13 @@ def main():
     print(model_df.dim_data())  # prints the dimension of the data set
     model_df.missing_data_ratio_print()  # prints the percentage of missing values in the data set (NONE FOUND!)
 
-    model_df.year_new_column("FinancialYear")  # create a new column for the year named: "Year"
+    #  calls a function to combine the column VehicleType and Manufacturer to a new column named
+    #  ManufacturerAndVehicleType
+    model_df.combine_columns("ManufacturerAndVehicleType", "VehicleType", "Manufacturer")
 
-    model_df._data_set["ManufacturerAndVehicleType"] = model_df._data_set["VehicleType"].map(str) + " " + model_df._data_set["Manufacturer"].map(str)
-    print(model_df._data_set.head())
+    print(model_df._data_set.head())  # print the data_set to check it worked correctly
 
-    #model_df._data_set.to_csv("Data_Out/data_set_out.csv")  # saves the pandas data frame to a CSV file
+    #  model_df._data_set.to_csv("Data_Out/data_set_out.csv")  # saves the pandas data frame to a CSV file
 
     model_df.box_plot("ConditionScore", "FinancialYear")  # box plot of the Condition score for each Year
     model_df.box_plot("ConditionScore", "Manufacturer")  # box plot of the Condition score for each Manufacturer
@@ -28,7 +31,9 @@ def main():
     model_df.bar_graph_percentage("FinancialYear")
 
     #  function that plots the percentage difference of what year each inspection occurs.
-    model_df.bar_graph_percentage_difference("FinancialYear")
+    model_df.line_graph_percentage_difference("FinancialYear")
+
+    model_df.triple_stacked_bar_graph('FinancialYear', 'VehicleType')
 
 
 if __name__ == "__main__":
